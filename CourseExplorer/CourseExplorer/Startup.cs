12cbeff0ad5077db07c432789757dc4e43778136
kyleloyd <kyle.loyd5953@outlook.com>
2015-05-20 07:16:40 -0400

    Initial commit
    Default MVC C#/ASP.NET application

---
+++ CourseExplorer/CourseExplorer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourseExplorer.Startup))]
+namespace CourseExplorer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
